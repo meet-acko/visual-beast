f1cb786169a8d027ca80c836be42eb9b085b6226
meet marakana <meet.marakana@acko.tech>
2024-04-28 17:41:24 +0530

    added context and fixed soft assert

--- utils/softAssert.js
+++ utils/softAssert.js
@@ -3,6 +3,7 @@ const playwright = require('@playwright/test');
 class SoftAssert {
     constructor() {
         this.errors = [];
+        this.errorMap = {};
     }
 
     async assert(condition, message) {
@@ -18,7 +19,9 @@ class SoftAssert {
         try {
             await expect(await image).toMatchImageSnapshot();
         } catch (error) {
-            await this.errors.push(error);
+            this.errorMap[await global.currentTestCaseName] = await error;
+            await this.errors.push(await this.errorMap);
+            this.errorMap = {};
         }
     }
 
@@ -26,13 +29,15 @@ class SoftAssert {
         try {
             await playwright.expect(await page).toHaveScreenshot({ fullPage: true });
         } catch (error) {
-            await this.errors.push(error);
+            this.errorMap[await global.currentTestCaseName] = await error;
+            await this.errors.push(await this.errorMap);
+            this.errorMap = {};
         }
     }
 
     async verify() {
         if (this.errors.length > 0) {
-            const errorMsg = await this.errors.map((err, index) => `Error ${index + 1}: ${err.message}`).join('\n');
+            const errorMsg = await this.errors.map((err, index) => `Error ${index + 1}: ${JSON.stringify((err[(Object.keys(err))[0]]).message)}`).join('\n');
             throw await new Error(`Soft Assert Errors:\n${errorMsg}`);
         }
     }
